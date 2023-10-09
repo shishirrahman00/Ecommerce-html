12b7bf0c4ac48ef878efcf9a489b8b949804fc72
Shishir Rahman Shuvo <shishirrahman23@gmail.com>
2023-10-09 15:11:36 +0600

    Add slick silder

---
+++ assetes/js/main.js
@@ -0,0 +1,36 @@
+$(".slick-slider").slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 300,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
